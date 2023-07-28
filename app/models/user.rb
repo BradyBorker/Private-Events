7d59f7afecc1f661c5b740a2d5e03f2220da3d53
Brady Barker <brady.d.barker@gmail.com>
2023-07-27 21:43:32 -0400

    Updated user association: has_many :created_events

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :events, foreign_key: :creator_id, dependent: :destroy
+  has_many :created_events, class_name: 'Event', foreign_key: :creator_id, dependent: :destroy
 end
