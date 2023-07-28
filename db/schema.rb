197c270c68530a000dcf3c7579abd71109aa0976
Brady Barker <brady.d.barker@gmail.com>
2023-07-27 21:09:44 -0400

    Added creator_id to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_28_002616) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_28_010444) do
   create_table "events", force: :cascade do |t|
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "body"
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
